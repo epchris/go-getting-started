c72d435138dec8c2ec9c0f2e6fe3c09ee5f4b540
Chris TenHarmsel <ctenharmsel@salesforce.com>
2025-10-28 10:36:25 -0400

    outputting logs

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
@@ -24,6 +25,16 @@ func main() {
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
 	})
+
+	// Emit a log line to stdout every minute
+	ticker := time.NewTicker(time.Minute)
+	defer ticker.Stop()
+	heartbeatLogger := log.New(os.Stdout, "", log.LstdFlags)
+	go func() {
+		for range ticker.C {
+			heartbeatLogger.Println("minute tick")
+		}
+	}()
 
 	router.Run(":" + port)
 }
